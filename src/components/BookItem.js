a67cdc6188c9daa75b642ac08b794ea336fd5cd5
stucodegreen <m.kuznecovas@gmail.com>
2020-03-14 22:56:47 +0000

    update

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,61 +1,65 @@
 import React from 'react';
 import './BookItem.css';
 
-
 export default class BookItem extends React.Component {
-	constructor(props){
-		super(props)
-		this.state = {
-			id: '',
-			title: '',
-			subtitle: '',
-			authors: '',
-			description:'',
-			pageCount:null,
-			thumbnail:'',
-			select: false
-		}
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: '',
+      title: '',
+      subtitle: '',
+      authors: '',
+      description: '',
+      pageCount: null,
+      thumbnail: '',
+      select: false
+    };
 
-		this.select = this.select.bind(this);
-	}
+    this.select = this.select.bind(this);
+  }
 
-	async componentDidMount() {
-		const {title, subtitle, authors, description, pageCount,imageLinks} = this.props.volumeInfo;
-		const {id} = this.props;
-
-		this.setState({
-			title,
-			subtitle,
-			authors:authors.join(', '),
-			description,
-			pageCount,
-			thumbnail:imageLinks.thumbnail,
-			select: JSON.parse(localStorage.getItem(`saveSelected${id}`))
-		});
-
+  async componentDidMount() {
+    const {
+      title,
+      subtitle,
+      authors,
+      description,
+      pageCount,
+      imageLinks
+		} = this.props.volumeInfo;
 		
-		var retrievedObject = localStorage.getItem('saveSelected');
-		console.log('retrievedObject: ',retrievedObject);
-	}
-	
-	select(e){
-		const currentSelect = this.state.select;
-		this.setState({select: !currentSelect});
-		localStorage.setItem(`saveSelected${this.props.id}`, !currentSelect);
-	}
+    const { id } = this.props;
+
+    this.setState({
+      title,
+      subtitle,
+      authors: authors.join(', '),
+      description,
+      pageCount,
+      thumbnail: imageLinks.thumbnail,
+      select: JSON.parse(localStorage.getItem(`saveSelected${id}`))
+    });
+  }
+
+  select() {
+    const currentSelect = this.state.select;
+    this.setState({ select: !currentSelect });
+    localStorage.setItem(`saveSelected${this.props.id}`, !currentSelect);
+  }
 
   render() {
     return (
-	<div onClick={this.select} className={this.state.select ? 'selected' : null}>
-	<p>{this.state.title}</p>
-	<p>{this.state.subtitle}</p>
-	<p>{this.state.authors}</p>
-	<p>{this.state.description}</p>
-	<p>{this.state.pageCount}</p>
-	<img src={this.state.thumbnail} alt=""/>
-
-			</div>
-	
+      <div
+        onClick={this.select}
+        className={this.state.select ? 'selected' : null}
+      >
+        <p>{this.state.title}</p>
+        <p>{this.state.subtitle}</p>
+        <p>{this.state.authors}</p>
+        <p>{this.state.description}</p>
+        <p>{this.state.pageCount}</p>
+        <img src={this.state.thumbnail} alt="" />
+      </div>
     );
   }
 }
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -4,56 +4,43 @@ import BookItem from './BookItem';
 import FeaturedItem from './FeaturedItem';
 
 class BookList extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			url: 'https://www.googleapis.com/books/v1/volumes?q=HTML5',
-			books: [],
-			featured:[],
-		};
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: 'https://www.googleapis.com/books/v1/volumes?q=HTML5',
+      books: [],
+      featured: []
+    };
+  }
 
-		// this.select = this.select.bind(this);
-	}
-
-	async componentDidMount() {
+  async componentDidMount() {
     const res = await axios.get(this.state.url);
-		this.setState({ 
-			books: res.data.items.slice(0,res.data.items.length-2),
-		 	featured:res.data.items.slice(res.data.items.length -2)
-		});
-		
-	}
+    this.setState({
+      books: res.data.items.slice(0, res.data.items.length - 2),
+      featured: res.data.items.slice(res.data.items.length - 2)
+    });
+  }
 
-	render(){
+  render() {
+    const books = this.state.books;
+    const featuredBooks = this.state.featured;
 
-		const books = this.state.books;
-		const featuredBooks = this.state.featured;
+    return (
+      <div>
+        {books.map(book => (
+          <BookItem key={book.id} volumeInfo={book.volumeInfo} id={book.id} />
+        ))}
 
-		return(
-				<div>
-				{books.map(book => 
-					<BookItem
-					key={book.id}
-					volumeInfo = {book.volumeInfo}
-					id={book.id}
-					/>							
-					)}
-	
-					{featuredBooks.map( featuredBook => 
-							<FeaturedItem
-							key={featuredBook.id}
-							volumeInfo = {featuredBook.volumeInfo}		
-							id= {featuredBook.id}			
-							/>
-						)}
-				</div>
-	
-		);
-	
-	}
-
-
+        {featuredBooks.map(featuredBook => (
+          <FeaturedItem
+            key={featuredBook.id}
+            volumeInfo={featuredBook.volumeInfo}
+            id={featuredBook.id}
+          />
+        ))}
+      </div>
+    );
+  }
 }
-
 
 export default BookList;
